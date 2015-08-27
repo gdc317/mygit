2772f4806e399cf3fef44f135813651b3333cc4b
lihr <wanshiruyi317@126.com>
2015-08-27 10:49:19 +0800

    add if

--- test01.py
+++ test01.py
@@ -2,3 +2,12 @@ print "hello, welcome to gitHub. my learn git"
 print "git client add hello git client"
 print "git client add hello ,second010"
 print "git server modified second"
+num=10
+print "Guess what i think?"
+answer=input()
+if answer<num:
+	print "to small!"
+if answer>num:
+	print "to big!"
+if answer==num:
+	print "BINGO!"
